a4d78e9be827dbc2fa3b453b601d053bb230850a
Mark Fine <mark.fine@gmail.com>
2015-03-04 18:02:13 -0800

    save schema.

--- db/schema.sql
+++ db/schema.sql
@@ -23,6 +23,19 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
+-- Name: hstore; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS hstore WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION hstore; Type: COMMENT; Schema: -; Owner: -
+--
+
+
+
+--
 -- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -35,20 +48,96 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
 
 
 
---
--- Name: hstore; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS "hstore" WITH SCHEMA public;
-
+SET search_path = public, pg_catalog;
 
 --
--- Name: EXTENSION "hstore"; Type: COMMENT; Schema: -; Owner: -
+-- Name: update_txid_column(); Type: FUNCTION; Schema: public; Owner: -
 --
 
+CREATE FUNCTION update_txid_column() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+        BEGIN
+           NEW.txid = txid_current();
+           RETURN NEW;
+        END;
+        $$;
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: devices; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE devices (
+    uuid uuid DEFAULT uuid_generate_v4() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone
+);
+
+
+--
+-- Name: records; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE records (
+    uuid uuid DEFAULT uuid_generate_v4() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone,
+    device_id uuid NOT NULL,
+    data hstore NOT NULL,
+    txid bigint DEFAULT txid_current()
+);
+
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE schema_migrations (
+    filename text NOT NULL
+);
+
+
+--
+-- Name: devices_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY devices
+    ADD CONSTRAINT devices_pkey PRIMARY KEY (uuid);
+
+
+--
+-- Name: records_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY records
+    ADD CONSTRAINT records_pkey PRIMARY KEY (uuid);
+
+
+--
+-- Name: schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY schema_migrations
+    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (filename);
+
+
+--
+-- Name: update_records_txid; Type: TRIGGER; Schema: public; Owner: -
+--
+
+CREATE TRIGGER update_records_txid BEFORE UPDATE ON records FOR EACH ROW EXECUTE PROCEDURE update_txid_column();
 
 
 --
 -- PostgreSQL database dump complete
 --
 
+INSERT INTO "schema_migrations" ("filename") VALUES ('1425504596_create_records.rb');
+INSERT INTO "schema_migrations" ("filename") VALUES ('1425505928_create_devices.rb');
+INSERT INTO "schema_migrations" ("filename") VALUES ('1425512651_moar_on_record.rb');
+INSERT INTO "schema_migrations" ("filename") VALUES ('1425518709_record-txid.rb');
